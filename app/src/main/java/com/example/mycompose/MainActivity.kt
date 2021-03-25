921aa0398b188a011801fc5f296340142fc96e0d
dededarirahmadi <dededarirahmadi@gmail.com>
2021-03-25 23:25:53 +0700

    learn about text

--- app/src/main/java/com/example/mycompose/MainActivity.kt
+++ app/src/main/java/com/example/mycompose/MainActivity.kt
@@ -3,36 +3,44 @@ package com.example.mycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mycompose.ui.theme.MyComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp(){
+    MyComposeTheme() {
+        Surface(color = Color.Cyan) {
+            Greeting("DEDE DARI RAHMADI")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello world $name!")
+        Text(text = "Hello world $name!",
+        modifier = Modifier.padding(24.dp)
+            )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyComposeTheme {
-        Greeting("Android")
-    }
+    MyApp()
 }
