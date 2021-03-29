5241d176c4b4c416e3c6c208e228cd6b7264bfb5
dededarirahmadi <dededarirahmadi@gmail.com>
2021-03-29 23:34:12 +0700

    learn mutableStateOf

--- app/src/main/java/com/example/mycompose/MainActivity.kt
+++ app/src/main/java/com/example/mycompose/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,6 +38,10 @@ fun MyApp(){
             ShowImage()
             Spacer(Modifier.height(16.dp))
             Greeting("DEDE DARI RAHMADI")
+            Spacer(Modifier.height(16.dp))
+            ShowList()
+            Spacer(Modifier.height(16.dp))
+            ClickCount()
         }
     }
 }
@@ -48,7 +57,6 @@ fun Greeting(name: String) {
             maxLines = 2,
             overflow = TextOverflow.Ellipsis
             )
-
 }
 
 @Composable
@@ -60,6 +68,24 @@ fun ShowImage(){
             .fillMaxWidth()
             .clip(shape = RoundedCornerShape(10.dp))
         )
+}
+
+@Composable
+fun ShowList(listNama: List<String> = listOf("Dede", "Dari", "Rahmadi")){
+    Column {
+        for (list in listNama){
+            Text(list)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun ClickCount(){
+    val count = remember { mutableStateOf(0)}
+    Button(onClick = {count.value++}) {
+        Text("sudah diclick ${count.value} kali")
+    }
 }
 
 @Preview(showBackground = true)
