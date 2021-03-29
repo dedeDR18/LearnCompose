4b37dcd85efe5bf4e17642652501e250f4602ff5
dededarirahmadi <dededarirahmadi@gmail.com>
2021-03-29 22:44:42 +0700

    done with jetpack compose basic

--- app/src/main/java/com/example/mycompose/MainActivity.kt
+++ app/src/main/java/com/example/mycompose/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -14,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mycompose.ui.theme.MyComposeTheme
@@ -30,9 +29,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(){
     MyComposeTheme() {
-
         Column( modifier = Modifier.padding(24.dp)) {
             ShowImage()
+            Spacer(Modifier.height(16.dp))
             Greeting("DEDE DARI RAHMADI")
         }
     }
@@ -41,9 +40,14 @@ fun MyApp(){
 @Composable
 fun Greeting(name: String) {
     val typography = MaterialTheme.typography
-        Text(text = "Hello world $name!", style = typography.h6)
+
+        Text(text = "Hello $name!", style = typography.h6)
         Text(text = "Selamat Datang !!!", style = typography.body2)
-        Text(text = "Kita Cobain Compose yuk..", style = typography.body2)
+        Text(text = "Kita Cobain Compose yuk.. wkwkwkwkwkwkwkkwkwkwkwkwkwkwkwkwkwwkwkwkwkwkkwkwkwkwkwkwkwkwkwkwkkwkwkwkwkwkwkwkwkwkwkwkkwkwkwkwkwkwkasdasdasdkjashdkjashdkjhaskjdakshdjkahskdajsk",
+            style = typography.body2,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+            )
 
 }
 
